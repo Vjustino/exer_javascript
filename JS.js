740f9e4367c85200633730d2648cff27beebeb38
Vjuustino <127980581+Vjustino@users.noreply.github.com>
2023-10-01 18:07:34 -0300

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,10 @@
+var n1 = parseInt(prompt('Digite um número:'));
+
+
+
+if( n1 % 2 === 0 && n1 >= 0 && n1 <= 100) {
+    alert('O número atende aos critérios')
+}
+else{
+    alert('O número não atende aos critérios, digite novamente.')
+}
